fc222a16afd8e0d6d3106aa7b7b2dc1b407cb880
unknown <sofiapparreira@gmail.com>
2024-07-30 19:59:47 -0300

    modal

--- src/components/CardBook.jsx
+++ src/components/CardBook.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import booksData from "../../json/books.json";
 
 const CardBook = ({ filterParam }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedBook, setSelectedBook] = useState(null);
+
   const getFilteredLivros = () => {
     return booksData.slice().sort((a, b) => {
       if (filterParam === "A-Z") {
@@ -16,35 +19,70 @@ const CardBook = ({ filterParam }) => {
 
   const filteredBooks = filterParam ? getFilteredLivros() : booksData;
 
+  const handleCardClick = (book) => {
+    setSelectedBook(book);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedBook(null);
+  };
+
   return (
-    <div className="grid grid-cols-3 justify-around mx-auto">
-      {filteredBooks.map((book, id) => (
-        <div
-          className="bg-gray-800 py-4 px-4 rounded-md flex justify-left mx-8 mb-8 gap-4"
-          key={id}
-        >
-          <img className="rounded w-40" src={book.capa} alt="" />
-          <div>
-            <h1 className="text-xl font-bold mb-8 text-center mt-4">
-              {book.nome}
-            </h1>
+    <div>
+      <div className="grid grid-cols-3 justify-around mx-auto">
+        {filteredBooks.map((book, id) => (
+          <div
+            className="bg-gray-800 py-4 px-4 rounded-md flex justify-left mx-8 mb-8 gap-4 cursor-pointer"
+            key={id}
+            onClick={() => handleCardClick(book)}
+          >
+            <img className="rounded w-40" src={book.capa} alt="" />
+            <div>
+              <h1 className="text-xl font-bold mb-8 text-center mt-4">
+                {book.nome}
+              </h1>
 
-            <p className="py-1.5">
-              <span className="font-medium pr-2">Editora:</span>
-              {book.editora}
-            </p>
-            <p className="py-1.5">
-              <span className="font-medium pr-2">Autor:</span>
-              {book.autor}
-            </p>
+              <p className="py-1.5">
+                <span className="font-medium pr-2">Editora:</span>
+                {book.editora}
+              </p>
+              <p className="py-1.5">
+                <span className="font-medium pr-2">Autor:</span>
+                {book.autor}
+              </p>
 
+              <p className="py-1.5">
+                <span className="font-medium pr-2">Categoria:</span>
+                {book.categoria}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {isModalOpen && selectedBook && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 text-black">
+          <div className="bg-white p-8 rounded-md shadow-lg max-w-md mx-auto">
+            <h2 className="text-2xl font-bold mb-4">{selectedBook.nome}</h2>
             <p className="py-1.5">
-              <span className="font-medium pr-2">Categoria:</span>
-              {book.categoria}
+              <span className="font-medium pr-2">Onde comprar:</span>
+              {selectedBook.ondeComprar}
             </p>
+            <p className="py-1.5">
+              <span className="font-medium pr-2">Valor:</span>
+              {selectedBook.valor}
+            </p>
+            <button
+              className="mt-4 bg-blue-800 text-white py-1 px-8 rounded-md"
+              onClick={closeModal}
+            >
+              Fechar
+            </button>
           </div>
         </div>
-      ))}
+      )}
     </div>
   );
 };
